78fc586992f521fc0b2e942cc70a2f775daa8590
SafaaC <safaaacherak@gmail.com>
2025-01-15 12:58:11 +0530

    eureka server setup

--- eureka-server/src/main/java/com/online/learning/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/online/learning/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.online.learning.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  // This annotation enables Eureka Server functionality
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
