aff311771114ad1950ac8005903878d9c3b6802a
SafaaC <safaaacherak@gmail.com>
2025-01-15 17:34:14 +0530

    initial setup for payment

---
+++ payment-service/src/test/java/com/online/learning/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.learning.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
